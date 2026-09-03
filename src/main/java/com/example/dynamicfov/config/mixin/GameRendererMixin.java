d31729770e7a178c30405f95a60b56965c95bbcd
Diarmait <diarmaitlmao@outlook.com>
2026-09-03 10:29:31 +0530

    Fix missing imports for #7 and #8

--- src/main/java/com/example/dynamicfov/config/mixin/GameRendererMixin.java
+++ src/main/java/com/example/dynamicfov/config/mixin/GameRendererMixin.java
@@ -10,6 +10,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.client.render.RenderTickCounter;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin 
